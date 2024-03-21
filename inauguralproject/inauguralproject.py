eabd2a51a83edce4afe886de59037be0ade541a1
fdb716 <fdb716@alumni.ku.dk>
2024-03-21 12:31:39 +0100

    dfs

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -14,17 +14,17 @@ def square(x):
     y = x**2
     return y
 
-def maximize_consumer_A_utility_discrete(self, P1):
-    """Maximize consumer A's utility over a discrete set of prices P1."""
+def max_A_utility(self, P1):
+    """Maximize utility for consumer A given prices P1"""
     max_utility = float('-inf')
-    optimal_price = None
-    optimal_allocation_A = None
+    optimal_p_1 = None
+    optimal_consumption_A = None
     for p1 in P1:
-        x1B_star, x2B_star = self.demand_B(p1)
-        x1A_star, x2A_star = 1 - x1B_star, 1 - x2B_star
-        utility_A = self.utility_A(x1A_star, x2A_star)
+        x1_B_star, x2_B_star = self.demand_B(p1)
+        x1_A_star, x2_A_star = 1 - x1_B_star, 1 - x2_B_star
+        utility_A = self.utility_A(x1_A_star, x2_A_star)
         if utility_A > max_utility:
             max_utility = utility_A
-            optimal_price = p1
-            optimal_allocation_A = (x1A_star, x2A_star)
-    return optimal_price, optimal_allocation_A, max_utility
+            optimal_p_1 = p1
+            optimal_consumption_A = (x1_A_star, x2_A_star)
+    return optimal_p_1, optimal_consumption_A, max_utility
