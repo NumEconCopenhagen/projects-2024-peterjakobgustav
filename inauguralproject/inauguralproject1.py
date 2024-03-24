fb5d82cb52046d7102c6ce486c09e3355d32e179
fdb716 <fdb716@alumni.ku.dk>
2024-03-24 13:56:28 +0100

    asd

--- inauguralproject/inauguralproject1.py
+++ inauguralproject/inauguralproject1.py
@@ -49,7 +49,7 @@ class ExchangeEconomyClass:
     
     def market_clearing_error(self, p1):
         errors = []
-        for p1 in P1:
+        for p1 in p1_values:
             # Calculate errors by equations giving in the question
             error1 = x1_A_star - self.par.omega_1A + x1_B_star - (1 - self.par.omega_1A)
             error2 = x2_A_star - self.par.omega_2A + x2_B_star - (1 - self.par.omega_2A)
